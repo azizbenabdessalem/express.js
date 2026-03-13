03e1d68ed04c48a686ee9a0a087c3543ea8b3ccc
azizbenabdessalem <azizbenabdessalem02@gmail.com>
2026-03-13 23:28:41 +0800

    Update and rename set_template_et_pug to set_template_et_pug.js

---
+++ set_template_et_pug.js
@@ -0,0 +1,40 @@
+app.set("view engine", "pug");          
+app.set("views", "./views"); 
+
+Cela dit à Express :
+
+utilise Pug
+
+les templates sont dans le dossier views
+
+
+
+const express = require('express');                  
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  const data = ['Item 1', 'Item 2', 'Item 3'];
+  res.render('index', { items: data });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+Navigateur
+     ↓
+Express route
+     ↓
+data = ['Item1','Item2','Item3']
+     ↓
+Pug template
+     ↓
+HTML généré
+     ↓
+page affichée
+
+Il yaura quelque chose comme ca dans pug :
+each item in items
+  li= item
